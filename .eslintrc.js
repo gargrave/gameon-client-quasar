ae8c64a3b339134dc367bdb13cb5ced063f26743
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-11 11:25:49 -0700

    ESLint rule fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
     'arrow-parens': 0,
     'one-var': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
