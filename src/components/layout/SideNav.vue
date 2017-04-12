f6d34446df1b96b68eff7055828423eda3fe6dbb
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-12 14:25:50 -0700

    Update SideNav to show/hide menus based on auth state

--- src/components/layout/SideNav.vue
+++ src/components/layout/SideNav.vue
@@ -9,20 +9,30 @@
         </div>
       </div>
 
-      <hr>
-      <div class="list-label">Navigation</div>
-      <q-drawer-link icon="games" :to="{ name: routes.games.list }">My Games</q-drawer-link>
-      <q-drawer-link icon="radio" :to="{ name: routes.platforms.list }">My Platforms</q-drawer-link>
+      <div v-if="isLoggedIn">
+        <hr>
+        <div class="list-label">Navigation</div>
+        <q-drawer-link icon="games" :to="{ name: routes.games.list }">My Games</q-drawer-link>
+        <q-drawer-link icon="radio" :to="{ name: routes.platforms.list }">My Platforms</q-drawer-link>
+      </div>
 
       <hr>
       <div class="list-label">Account</div>
-      <q-drawer-link icon="radio" :to="{ name: routes.auth.detail }">My Account</q-drawer-link>
+      <div v-if="isLoggedIn">
+        <q-drawer-link icon="account_circle" :to="{ name: routes.auth.detail }">My Account</q-drawer-link>
+      </div>
+      <div v-else>
+        <q-drawer-link icon="account_circle" :to="{ name: routes.auth.login }">Log In</q-drawer-link>
+        <q-drawer-link icon="add_circle_outline" :to="{ name: routes.auth.create }">Sign Up</q-drawer-link>
+      </div>
 
     </div>
   </q-drawer>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 import { routes } from '../../globals/urls'
 
 export default {
@@ -30,6 +40,12 @@ export default {
     return {
       routes // expose to template
     }
+  },
+
+  computed: {
+    ...mapGetters([
+      'isLoggedIn'
+    ])
   }
 }
 </script>
