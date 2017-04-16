556afcab7ffed94b08dba72ae58a9bc56bdc83c8
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-16 11:13:56 -0700

    Add collection of pre-defined Toast objects

---
+++ src/globals/toasts.js
@@ -0,0 +1,11 @@
+/*
+A set of pre-defined Toast objects for use with Quasar.
+Simply a convenience to avoid writing the same Toast data over and over.
+*/
+import { Toast } from 'quasar'
+
+export default {
+  noChanges () {
+    Toast.create.warning('Nothing to save! No data has been changed.')
+  }
+}
