7ac7892d53b8dea251950c6a67bce227b93b50ec
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-17 09:26:25 -0700

    Add helper method for Axios DELETE request

--- src/globals/utils.js
+++ src/globals/utils.js
@@ -61,5 +61,15 @@ export const apiHelper = {
     let req = this.axPost(url, data, authToken)
     req.method = 'put'
     return req
+  },
+
+  /**
+   * Builds a DELETE request for an AJAX call via axios.
+   * Note that this simply uses the POST method build, and then changes the method.
+   */
+  axDelete (url, data, authToken = null) {
+    let req = this.axPost(url, data, authToken)
+    req.method = 'delete'
+    return req
   }
 }
