6b7b2d42d28049934992cb23695bcde59bb1daf3
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-04-20 13:47:46 -0700

    Improve display on 'account detail' page

--- src/components/auth/containers/AccountDetailPage.vue
+++ src/components/auth/containers/AccountDetailPage.vue
@@ -9,17 +9,12 @@
             My Profile
           </div><!-- /card-title -->
 
-          <div class="card-content">
-            <p><strong>Name:</strong> {{ profile.firstName }} {{ profile.lastName }}</p>
-            <p><strong>Email:</strong> {{ userData.email }}</p>
-
-            <hr>
-            <button
-              class="secondary"
-              @click="onLogout">
-                Logout
-            </button>
-          </div><!-- /card-content -->
+          <app-account-detail-view
+            :userData="userData"
+            :profile="profile"
+            @editClicked="onEditClick"
+            @logoutClicked="onLogoutClick">
+          </app-account-detail-view>
 
         </div><!-- /card -->
 
@@ -34,11 +29,22 @@ import { Loading, Toast } from 'quasar'
 
 import { localUrls } from '../../../globals/urls'
 
+import AccountDetailView from '../components/AccountDetailView'
+
 export default {
+  components: {
+    appAccountDetailView: AccountDetailView
+  },
+
   data () {
     return {
+      initializing: true,
       // whether any operations are currently running
-      working: false
+      working: false,
+      // error messages returned from API (e.g. invalid data)
+      apiError: '',
+      // whether we are in editing or viewing mode
+      editing: false
     }
   },
 
@@ -50,7 +56,17 @@ export default {
   },
 
   methods: {
-    onLogout () {
+    /**
+     * Callback for clicking the 'edit' button; simply change to 'editing' state.
+     */
+    onEditClick () {
+      this.editing = true
+    },
+
+    /**
+     * Callback for clicking the 'logout' button; simply logout.
+     */
+    onLogoutClick () {
       this.working = true
       this.logout()
         .then(() => {
@@ -75,10 +91,12 @@ export default {
       .then(() => {
         Loading.hide()
         this.working = false
+        this.initializing = false
       }, () => {
         Loading.hide()
         this.$router.push(localUrls.login)
         this.working = false
+        this.initializing = false
       })
   }
 }
