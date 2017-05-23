0a8398ab4f057241c864464b0c91b9244938def0
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-05-23 14:41:20 -0700

    ESLint update -> only show warnings for unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     'one-var': 0,
+    'no-unused-vars': 1,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
