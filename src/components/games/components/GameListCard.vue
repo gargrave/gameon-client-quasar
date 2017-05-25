0cc87fae390efa6c89f2ab5664398462a6b2af15
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-05-25 08:47:07 -0700

    Update layout (Flex grid) for Game Cards

--- src/components/games/components/GameListCard.vue
+++ src/components/games/components/GameListCard.vue
@@ -4,9 +4,21 @@
     @click="$emit('click', { id: game.id })">
 
     <div class="card-content">
-      <h6>{{ game.title }}</h6>
-    </div><!-- /card-content -->
+      <div class="row justify-between">
 
+        <div class="column justify-center">
+          <h6 style="display: inline;">
+            {{ game.title }}
+          </h6>
+          <p>Last played: [TODO: show the date, ya dingus!]</p>
+        </div><!-- /column -->
+
+        <div class="column justify-center">
+          <span class="label bg-faded text-white ">{{ game.dates.length }}</span>
+        </div><!-- /column -->
+
+      </div><!-- /row -->
+    </div><!-- /card-content -->
   </div><!-- /card -->
 </template>
 
