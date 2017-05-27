6c9f06df764c2fe32aa7496003c4396409a3cf02
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-05-27 11:56:40 -0700

    Updated add/modified display for PlatformDetail

--- src/components/platforms/components/PlatformDetailView.vue
+++ src/components/platforms/components/PlatformDetailView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="card-content">
     <p><strong>Name:</strong> {{ platform.title }}</p>
-    <p><strong>Added on:</strong> {{ platform.created }}</p>
+
+    <hr>
+    <p><strong>Added on:</strong> {{ addedOnString }}</p>
+    <p><strong>Updated on:</strong> {{ updatedOnString }}</p>
 
     <hr>
     <button
@@ -19,10 +22,22 @@
 </template>
 
 <script>
+import dateHelper from '../../../utils/dateHelper'
+
 export default {
   props: {
      // the Platform object being viewed
     platform: { type: Object, required: true }
+  },
+
+  computed: {
+    addedOnString () {
+      return dateHelper.cleanDateWithTrailingDay(this.platform.created)
+    },
+
+    updatedOnString () {
+      return dateHelper.cleanDateWithTrailingDay(this.platform.modified)
+    }
   }
 }
 </script>
